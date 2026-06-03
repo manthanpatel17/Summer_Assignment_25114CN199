620ce9f2264823eb5b8949da1ed1a6ab3c9fe7bb
manthanpatel17 <manthanboss17@gmail.com>
2026-06-03 18:52:07 +0530

    Add files via upload

---
+++ DAY3/PrimeNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber
+{
+    public static void main(String args[]) 
+    
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer");
+        int n = sc.nextInt();
+        int c = 0;
+
+        for (int i = 1; i <= n; i++) 
+        {
+            if (n % i == 0) 
+            {
+                c++;
+            }
+        }
+
+        if (c == 2) 
+            {
+            System.out.println(n + " is a prime number");
+        }
+        else 
+            {
+            System.out.println(n + " is not a prime number");
+        }
+    }
+}
