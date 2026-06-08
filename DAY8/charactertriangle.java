7e8e02419961c4154392480a499b4c0d2efdcc6c
manthanpatel17 <manthanboss17@gmail.com>
2026-06-08 17:58:34 +0530

    Create charactertriangle.java

---
+++ DAY8/charactertriangle.java
@@ -0,0 +1,11 @@
+public class CharacterTriangle {
+  public static void main(String[] args) {
+
+    for (int i = 'A'; i <= 'E'; i++) {
+      for (int j = 'A'; j <= i; j++) {
+        System.out.print((char) j);
+      }
+      System.out.println();
+    }
+  }
+}
