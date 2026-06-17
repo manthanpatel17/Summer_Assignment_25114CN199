792846071469ee61aa221ac094b1c778b7d2d3d3
manthanpatel17 <manthanboss17@gmail.com>
2026-06-17 19:17:21 +0530

    Add files via upload

---
+++ DAY15/rotatearrayleft.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class RotateArrayLeft
+
+ {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" enter the size of the array");
+    int size = sc.nextInt();
+
+    if (size <= 0) {
+      System.out.println(" invalid entry");
+      sc.close();
+      return;
+    }
+    int[] arr1 = new int[size];
+    int[] arr2 = new int[size];
+
+    System.out.println(" enter the elements of the array");
+    for (int i = 0; i < size; i++) {
+      arr1[i] = sc.nextInt();
+    }
+    System.out.println(" enter the no. of rotation");
+    int num = sc.nextInt();
+    num = num % size;
+
+    for (int i = 0; i < size; i++) {
+      arr2[i] = arr1[(i + num) % size];
+    }
+
+    System.out.println(" the rotated array is :");
+    for (int i = 0; i < size; i++) {
+      System.out.print(arr2[i] + " ");
+    }
+
+    sc.close();
+  }
+}
