04041c11d00dc693de378229bb25f1941e145e22
manthanpatel17 <manthanboss17@gmail.com>
2026-06-19 19:36:18 +0530

    Add files via upload

---
+++ DAY19/matricesadd.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MatricesAdd {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(" enter the rows of the matrix ");
+    int rows = sc.nextInt();
+    System.out.println(" enter the column of the matrix ");
+    int colm = sc.nextInt();
+
+    if (rows <= 0 || colm <= 0) {
+      System.out.println("Invalid matrix size");
+      sc.close();
+      return;
+    }
+
+    int[][] nums1 = new int[rows][colm];
+    int[][] nums2 = new int[rows][colm];
+
+    int[][] add = new int[rows][colm];
+
+    System.out.println(" enter the elements of the matrix 1 ");
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < colm; j++) {
+        nums1[i][j] = sc.nextInt();
+      }
+    }
+
+    System.out.println(" enter the elements of the matrix 2 ");
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < colm; j++) {
+        nums2[i][j] = sc.nextInt();
+      }
+    }
+    System.out.println(" the addition of the two matrix is :");
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < colm; j++) {
+        add[i][j] = nums1[i][j] + nums2[i][j];
+        System.out.print(add[i][j] + " ");
+      }
+      System.out.println();
+
+    }
+    sc.close();
+  }
+}
