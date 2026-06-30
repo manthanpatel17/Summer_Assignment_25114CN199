33b26813fd51936aabb45bb5448fc82194c3c979
manthanpatel17 <manthanboss17@gmail.com>
2026-06-30 20:00:19 +0530

    Add files via upload

---
+++ DAY30/MiniEmployeeMnagementSystem.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MiniEmployeeMnagementSystem {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter number of employees: ");
+    int n = sc.nextInt();
+
+    int[] empId = new int[n];
+    String[] empName = new String[n];
+    String[] department = new String[n];
+    double[] salary = new double[n];
+
+    for (int i = 0; i < n; i++) {
+      System.out.println("\nEnter details of Employee " + (i + 1));
+
+      System.out.print("Employee ID: ");
+      empId[i] = sc.nextInt();
+
+      System.out.print("Employee Name: ");
+      empName[i] = sc.next();
+
+      System.out.print("Department: ");
+      department[i] = sc.next();
+
+      System.out.print("Salary: ");
+      salary[i] = sc.nextDouble();
+    }
+
+    System.out.println("\n----- EMPLOYEE RECORDS -----");
+
+    for (int i = 0; i < n; i++) {
+      System.out.println("\nEmployee " + (i + 1));
+      System.out.println("ID         : " + empId[i]);
+      System.out.println("Name       : " + empName[i]);
+      System.out.println("Department : " + department[i]);
+      System.out.println("Salary     : " + salary[i]);
+    }
+    sc.close();
+  }
+}
