3046316d880e8dbcbce6a3025561fce14477a942
manthanpatel17 <manthanboss17@gmail.com>
2026-06-30 19:59:54 +0530

    Add files via upload

---
+++ DAY30/minilibrarysystem.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MiniLibrarySystem {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String[] books = { "One Piece", "Attack On Titan", "Naruto", "DC", "Marvel" };
+    boolean[] issued = new boolean[5];
+
+    System.out.println("Available Books:");
+    for (int i = 0; i < books.length; i++) {
+      System.out.println((i + 1) + ". " + books[i]);
+    }
+    for (int i = 0; i < books.length; i++) {
+      System.out.print("Enter book number to issue: ");
+      int choice = sc.nextInt();
+
+      if (choice >= 1 && choice <= books.length) {
+        if (!issued[choice - 1]) {
+          issued[choice - 1] = true;
+          System.out.println("Book issued successfully: " + books[choice - 1]);
+        } else {
+          System.out.println("Book is already issued.");
+          break;
+        }
+      } else {
+        System.out.println("Invalid book number.");
+        break;
+      }
+    }
+    sc.close();
+  }
+}
